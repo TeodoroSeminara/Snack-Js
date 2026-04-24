2884624829a8c4e47cd9f67e0eb41c68efb89cf8
Teodoro <seminara.teodoro93@gmail.com>
2026-04-24 12:16:05 +0200

    snack 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Dato un array di numeri, stampa solo i numeri pari.
+//1 Dato un array di numeri, stampa solo i numeri pari.
 const x = [1, 2, 3, 4, 5, 6];
 const y = [];
 for (i = 0; i < x.length; i++) {
@@ -9,16 +9,20 @@ for (i = 0; i < x.length; i++) {
 
 console.log(y);
 
-
-// Dato un array di numeri, crea un nuovo array con i numeri maggiori di 10.
+//2 Dato un array di numeri, crea un nuovo array con i numeri maggiori di 10.
 const arrayEser2 = [4, 12, 7, 25, 3, 18];
 const arrayOver10 = [];
 
-for(i = 0; i < arrayEser2.length; i++){
-  if(arrayEser2[i] > 10){
-    arrayOver10.push(arrayEser2[i])
+for (i = 0; i < arrayEser2.length; i++) {
+  if (arrayEser2[i] > 10) {
+    arrayOver10.push(arrayEser2[i]);
   }
 }
 
 console.log(arrayOver10);
 
+const test = arrayEser2.filter((x) => x > 10);
+
+console.log(test);
+
+//3 Dato un array di stringhe, restituisci un array con la lunghezza di ogni parola.
